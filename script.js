d0bd59965f1cabf4ffb4056bff4d6a13cbcddf89
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2024-09-22 11:00:24 +0100

    2024.09.22

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+//Method 1
+
+function getMessage() {
+    
+    const input = document.getElementById("message-input").value
+
+    document.getElementById("message-output").innerHTML = input;
+
+    document.getElementById("message-input").value = "";
+}
+
+
+//Method 2
+
+// const messageInput = document.getElementById("message-input")
+
+// function getMessage() {
+//     document.getElementById("message-output").innerHTML = messageInput.value;
+
+//     messageInput.value="";
+// }
